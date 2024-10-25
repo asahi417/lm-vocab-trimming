86dd5797e12f29c940858c5c2f0c07aee90350fe
asahi417 <asahi1992ushio@gmail.com>
2024-10-25 14:55:01 +0900

    init

--- experiments_zero/train_vocab_embedding.py
+++ experiments_zero/train_vocab_embedding.py
@@ -26,7 +26,7 @@ class Trainer:
                  parameter_prefix: str = "embeddings",
                  torch_dtype: torch.dtype = torch.bfloat16,
                  random_seed: int = 42,
-                 weight_decay: float = 1e-8,
+                 weight_decay: float = 1e-7,
                  lr: float = 0.00002,
                  lr_warmup: int = 100):
         # config
